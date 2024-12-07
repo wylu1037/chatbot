ef4de029edbeff8abe6617bedc4ee65153163a39
wylu1037 <wylu1037@gmail.com>
2024-12-07 20:29:54 +0800

    feat: add middleware for authentication in API routes
    - Implemented a new middleware function to handle user authentication.
    - Redirects unauthenticated users to the signin page.
    - Configured to match API routes for session validation.

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { auth } from "./server/auth";
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  const session = await auth();
+  if (!session) {
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/api/:path*"],
+};
